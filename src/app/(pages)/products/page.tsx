0c117f0d8d59253eec6a60c046b68d7168a2fb4f
hayyrs66 <rayrtsx@proton.me>
2024-01-03 18:44:20 -0600

    project implemented

---
+++ src/app/(pages)/products/page.tsx
@@ -0,0 +1,39 @@
+import { draftMode } from 'next/headers';
+import { Category, Page } from '../../../payload/payload-types';
+import { fetchDoc } from '../../_api/fetchDoc';
+import { Blocks } from '../../_components/Blocks';
+import { Gutter } from '../../_components/Gutter';
+import Filters from './Filters';
+import classes from './index.module.scss';
+import { fetchDocs } from '../../_api/fetchDocs';
+import { HR } from '../../_components/HR';
+
+async function Products() {
+    const { isEnabled: isDraftMode } = draftMode();
+    
+    let page: Page | null = null
+    let categories: Category[] | null = null;
+
+    try {
+        page = await fetchDoc<Page>({
+            collection: 'pages',
+            slug: 'products',
+            draft: isDraftMode
+        })
+
+        categories = await fetchDocs<Category>('categories');
+    } catch (error) {
+        console.log(error)
+    }
+  return (
+    <div className={classes.container}>
+        <Gutter className={classes.products}>
+            <Filters categories={categories} />
+            <Blocks blocks={page.layout} disableTopPadding={true} />
+        </Gutter>
+        <HR/>
+    </div>
+  )
+}
+
+export default Products
