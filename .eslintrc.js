7168751867c83f6d677d7698f20c2c293d329b1f
hayyrs66 <rayrtsx@proton.me>
2024-01-04 19:27:46 -0600

    ignoring linter and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
+  ignorePatterns: ['**/payload-types.ts', '**/*'],  // Agregado '**/*' para ignorar todo
   plugins: ['prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
     'no-console': 'off',
   },
-}
+};
